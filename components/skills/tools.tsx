6a029fc0d8a78706838e192ac6a6b4508362c35e
Santosh Bhandari <santooshbhandari88@gmail.com>
2026-09-05 23:59:19 +0545

    manage tools that i used

---
+++ components/skills/tools.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import AboutCard from "../common/cards/about.card";
+
+const Tools = () => {
+  return (
+    <main>
+      <div>
+        <AboutCard icon name="Git" description="" />
+        <AboutCard icon name="GitHub" description="" />
+        <AboutCard icon name="PostMan" description="" />
+      </div>
+    </main>
+  );
+};
+
+export default Tools;
