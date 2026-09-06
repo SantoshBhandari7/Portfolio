166b19ee607a4d2067628edb07bb2e28e3a52cd2
Santosh Bhandari <santooshbhandari88@gmail.com>
2026-09-06 13:03:29 +0545

    about section

--- components/about/page.tsx
+++ components/about/page.tsx
@@ -3,7 +3,7 @@ import React, { useState } from "react";
 import Button from "../common/ui/button";
 import AboutCard from "../common/cards/about.card";
 import { FaReact } from "react-icons/fa6";
-import { SiNextdotjs } from "react-icons/si";
+import { SiNextdotjs, SiSkillshare } from "react-icons/si";
 import {
   TbBrandCss3,
   TbBrandHtml5,
@@ -12,6 +12,10 @@ import {
 } from "react-icons/tb";
 import { BiLogoTailwindCss } from "react-icons/bi";
 import SkillTabs from "../skills/skilltabs";
+import { MdCastForEducation } from "react-icons/md";
+import { AiFillExperiment } from "react-icons/ai";
+import Experiance from "../experiance/page";
+import Education from "../education/page";
 
 const AboutSection = () => {
   const [about, setAbout] = useState("skills");
@@ -26,7 +30,7 @@ const AboutSection = () => {
               : "rounded-full border-gray-700"
           }
         >
-          Skills
+          <SiSkillshare /> Skills
         </div>
         <div
           onClick={() => setAbout("experience")}
@@ -35,7 +39,26 @@ const AboutSection = () => {
               ? "rounded-full from-blue-500 to-purple-500 text-lg"
               : "rounded-full border-gray-600"
           }
-        ></div>
+        >
+          <AiFillExperiment /> Experiance
+        </div>
+
+        <div
+          onClick={() => setAbout("education")}
+          className={
+            about === "education"
+              ? "rounded-full from-blue-500 to-purple-500 text-lg"
+              : "rounded-full border-gray-600"
+          }
+        >
+          <MdCastForEducation /> Education
+        </div>
+
+        <div>
+          {about === "skills" && <SkillTabs />}
+          {about === "experiance" && <Experiance />}
+          {about === "education" && <Education />}
+        </div>
       </section>
     </main>
   );
